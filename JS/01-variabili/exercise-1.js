d4870f2dbf4d4a44e345b011cd1aa6a453de1727
stefaniazocco <stefaniazocco93@gmail.com>
2022-07-17 15:45:40 +0200

    fix error

--- JS/01-variabili/exercise-1.js
+++ JS/01-variabili/exercise-1.js
@@ -1,11 +1,8 @@
 function canPlay() {
   if (true) {
-    let personName = 'Paul';
-  }
-
-  personName += ' plays football'; // Argh! personName is not defined
-
-  console.log(personName);
+     let personName = "Paul";
+     
+     personName += "plays football"; // Argh! personName is not defined  
 }
-
+  }
 canPlay();

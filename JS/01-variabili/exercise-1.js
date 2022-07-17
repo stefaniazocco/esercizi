9060208a647d561de50b1519d1a85261df1d816f
stefaniazocco <stefaniazocco93@gmail.com>
2022-07-17 14:32:14 +0200

    add files

---
+++ JS/01-variabili/exercise-1.js
@@ -0,0 +1,11 @@
+function canPlay() {
+  if (true) {
+    let personName = 'Paul';
+  }
+
+  personName += ' plays football'; // Argh! personName is not defined
+
+  console.log(personName);
+}
+
+canPlay();

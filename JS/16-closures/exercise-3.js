a640031b8955b7cd9549fd2a733fce4f9182c8ae
stefaniazocco <stefaniazocco93@gmail.com>
2022-07-22 16:59:50 +0200

    add exercise 16

---
+++ JS/16-closures/exercise-3.js
@@ -0,0 +1,20 @@
+function createStore() {
+  let Store = [];
+    return function(item){
+      Store.push(item);
+      console.log(Store);
+    }
+}
+
+const redPants = { id: 1, name: 'Red Pants' };
+const whiteHat = { id: 2, name: 'White Hat' };
+const blackSneakers = { id: 3, name: 'Black Sneakers' };
+
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat);
+
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);

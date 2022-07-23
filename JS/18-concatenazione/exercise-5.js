d7960992f4abb9fc7afde4b4123201c93756f56b
stefaniazocco <stefaniazocco93@gmail.com>
2022-07-23 16:44:40 +0200

    add function to exercise 18

---
+++ JS/18-concatenazione/exercise-5.js
@@ -0,0 +1,27 @@
+function memoize(fn) {
+  let cache = {};
+  return (num) => {
+    console.log(`Controllo cache per numero: ${num}`);
+    if (num in cache) {
+      return (`Numero ${num} già  presente nella cache, fattoriale: ${cache[num]}`);
+    }
+    else {
+      cache[num] = fn(num);
+      return (`Numero ${num} non presente, fattoriale: ${fn(num)}`)
+    }
+  }
+}
+
+function factorial(x) {
+  if (x === 0) {
+    return 1;
+  }
+  return x * factorial(x - 1);
+}
+
+const fattoriale = memoize(factorial); //factorial = memoize(factorial)
+console.log(fattoriale(10));
+console.log(fattoriale(5));
+console.log(fattoriale(6));
+console.log(fattoriale(12));
+console.log(fattoriale(5));

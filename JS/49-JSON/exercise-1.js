7182426c35e0917c76564c883e2bb7ab55ebebf2
stefaniazocco <stefaniazocco93@gmail.com>
2022-08-12 14:13:40 +0200

    add exercise 49

---
+++ JS/49-JSON/exercise-1.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+let json= JSON.stringify(developer)
+console.log(json)
+// Print developer as json object

49f97e7378eaf3f6076921291e7575574eada923
stefaniazocco <stefaniazocco93@gmail.com>
2022-08-19 14:09:05 +0200

    fix 57

--- JS/57-async/exercise-1.js
+++ JS/57-async/exercise-1.js
@@ -20,22 +20,22 @@ const persons = [
 ];
 
 function fetchPersonById(id) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     if (persons.find((person) => person.id === id)){
       resolve(persons.find((person) => person.id === id))
-    } else {
-      reject (new Error("this id doesn't exist"))
-    }
+    } // else {
+    //   reject (new Error("this id doesn't exist"))
+    // }
   })
 }
 let uno = fetchPersonById(1);     //{ id: 1, firstName: 'Mario', lastName: 'Rossi', age: 25 }
 uno
 .then(console.log)              
-.catch(err =>
-  console.log("Error:", err.message))
+// .catch(err =>
+//   console.log("Error:", err.message))
 
-let quattro = fetchPersonById(4);   //Error: this id doesn't exist
-quattro
-.then(console.log)
-.catch(err =>
-  console.log("Error:", err.message))
+//let quattro = fetchPersonById(4);   //Error: this id doesn't exist
+// quattro
+// .then(console.log)
+// .catch(err =>
+//   console.log("Error:", err.message))

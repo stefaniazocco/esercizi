a85b3aea292da9a8afb1ca7762cbe00df5af93b9
stefaniazocco <stefaniazocco93@gmail.com>
2022-11-17 17:49:32 +0100

    add exercise 6

---
+++ Nodejs/6/04-03-server-json.mjs
@@ -0,0 +1,20 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "application/json");
+
+  const jsonResponseBody = JSON.stringify({ location: "Mars" });
+
+  response.end(jsonResponseBody);
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
+
+// "Earth" => Content-Length: 20
+// "Mars" => Content-Length: 19

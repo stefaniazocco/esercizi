340f5128104f12dc0534776115f26287d34ca4f5
stefaniazocco <stefaniazocco93@gmail.com>
2022-11-17 18:40:59 +0100

    add exercise 7

---
+++ Nodejs/7/script.js
@@ -0,0 +1,24 @@
+var figlet = require('figlet');
+
+figlet.text('Stefania', {
+    font: 'Hieroglyphs',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+figlet.fonts(function(err, fonts) {
+    if (err) {
+        console.log('something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.dir(fonts);
+});

dbe8b460cde4dd6eaadce2fc74496a26dd2bef4c
stefaniazocco <stefaniazocco93@gmail.com>
2022-11-19 19:22:48 +0100

    add exercise 11

---
+++ Nodejs/11/luckyDraw.js
@@ -0,0 +1,32 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+}
+
+
+luckyDraw("Joe")
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+
+    .then(() => luckyDraw("Caroline"))
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+    
+    .then(() => luckyDraw("Sabrina"))
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+
+
+    
+// const players = ['Joe', 'Carolina', 'Sabrina']
+// players.forEach( user => luckyDraw(user)
+//     .then(result => console.log(result))
+//     .catch(error=> console.log(error)))
